c980be51950a3adcf4cdfc6a3f01f8847081eaad
Gagan <50614855+maaynussih@users.noreply.github.com>
2025-11-22 18:16:25 +0530

    CLI implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <string>
+#include "LRUCache.hpp"
+#include "LFUCache.hpp"
 
-int main(){
-    std::cout<<"Cache System Ready\n";
+int main() {
+    std::string type;
+    std::cout << "Choose cacheType ('lru' or 'lfu'): ";
+    std::cin >> type;
+
+    ICache* cache = nullptr;
+    if (type == "lru") {
+        cache = new LRUCache(5);
+    }
+    else if (type == "lfu") {
+        cache = new LFUCache(5);
+    }
+    else {
+        std::cout << "Invalid cacheType \n";
+        return 0;
+    }
+
+    std::cout <<"Cache ready. Enter commands: \n";
+    std::cout <<"put <key> <value> \n";
+    std::cout <<"get <key> \n";
+    std::cout <<"exit \n";
+
+    while (true) {
+        std::string operation;
+        std::cout<<"> ";
+        std::cin >>operation;
+
+        if (operation == "put") {
+            int key, value;
+            std::cin>>key;
+            std::cin>>value;
+            cache->put(key,value);
+            std::cout<<"Inserted \n";
+        }
+        else if (operation == "get") {
+            int key;
+            std::cin>>key;
+            std::cout<<cache->get(key)<<"\n";
+        }
+        else if (operation == "exit") {
+            break;
+        }
+        else {
+            std::cout<<"Unknown command \n";
+        }
+    }
+
+    delete cache;
     return 0;
 }
